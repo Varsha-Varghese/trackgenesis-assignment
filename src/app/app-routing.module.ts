fea176964376b947c46dedfe53c19d072cd96603
Varsha-Varghese <varshavarghese0303@gmail.com>
2022-01-24 20:38:46 +0530

    trackgenesis assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LandingPageComponent } from "./../app/landing-page/landing-page.component";
+const routes: Routes = [
+  {
+    component: LandingPageComponent,
+    path: "home"
+  },
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './loading/loading.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
